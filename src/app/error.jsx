edae11db9c9bf3e560c50400a57df4cf9d9d5204
fatihsenyigit <fsenyigit8@gmail.com>
2025-05-31 19:10:10 +0700

    error page

---
+++ src/app/error.jsx
@@ -0,0 +1,18 @@
+
+'use client' // Error boundaries must be Client Components
+ 
+import { useEffect } from 'react'
+ 
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+ 
+  return (
+    <div>
+      <h2 className='text-3xl text-red-800'>Something went wrong!</h2>
+      
+    </div>
+  )
+}
