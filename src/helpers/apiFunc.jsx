c792a5b18a8499a6aef4bd1f38356491e7369007
fatihsenyigit <fsenyigit8@gmail.com>
2025-05-31 16:38:49 +0700

    revalidate

--- src/helpers/apiFunc.jsx
+++ src/helpers/apiFunc.jsx
@@ -1,24 +1,36 @@
-
 // next.js fetch api ile cekilen verileri default olarak cache'ler. bu ozelligi option objesi ile degistirebilirz
 // const res = await fetch(URL, {cache: 'force-cache'}) -- bu default
 // const res = await fetch(URL, {cache: 'no-store'}) -- bu cache leme
 // const res = await fetch(URL, {next: {revalidate: '10'}}) -- bu belirlenen saniye cinsinden sure sonunda veriyi tekrar cek
 
-const URL = 'http://localhost:8080/users'; 
+const URL = "http://localhost:8080/users";
 
+// // force-cache
+// export async function getUsers() {
+//     //by default, next.js automatically caches the returned values of fetch
+//     const res = await fetch(URL)
+//     // the return value is not serialized
+//     // you can return DATE, Map, Set, etc
 
-// force-cache
+//     if (!res.ok) {
+//         //this will activate the closest error.js Error Boundry
+//         throw new Error('failed to fetch data')
+//     }
+
+//     const data = await res.json()
+//     return data
+// }
+
+// revalidate
 export async function getUsers() {
-    //by default, next.js automatically caches the returned values of fetch
-    const res = await fetch(URL)
-    // the return value is not serialized
-    // you can return DATE, Map, Set, etc
+  // automatically revalidate data after a certain amount of time has passed. this is useful for data that changes infrequently and freshness is not as critical.
+  // to revalidate data at a timed interval, you can use the the next.revalidate option of fetch to set the cache lifetime of a resource(in seconds).
+  const res = await fetch(URL, { next: { revalidate: 20 } });
+  if (!res.ok) {
+    //this will activate the closest error.js Error Boundry
+    throw new Error("failed to fetch data");
+  }
 
-    if (!res.ok) {
-        //this will activate the closest error.js Error Boundry
-        throw new Error('failed to fetch data')
-    }
-
-    const data = await res.json()
-    return data
+  const data = await res.json();
+  return data
 }
